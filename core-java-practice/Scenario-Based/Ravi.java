5cdc681dec8142b6f9deb9e0cbdc9d8b0280fd56
suhani0110 <suhanitomar10@gmail.com>
2026-06-10 13:39:17 +0530

    [Suhani] Add: StoryBased Problem

---
+++ core-java-practice/Scenario-Based/Ravi.java
@@ -0,0 +1,18 @@
+public class Ravi {
+    public static void main(String[] args) {
+        String name = "Ravi";
+        int age = 22;
+        int rank = 5;
+        double salary = 50000;
+        float membershipFee = 1500.50f;
+        double bonus = salary * 0.12;
+        int bonusInt = (int) bonus;
+        System.out.println("    WELCOME CARD");
+        System.out.println("Name           : " + name);
+        System.out.println("Age            : " + age);
+        System.out.println("Rank           : " + rank);
+        System.out.println("Salary         : INR " + salary);
+        System.out.println("Membership Fee : INR " + membershipFee);
+        System.out.println("Annual Bonus   : INR " + bonusInt);
+    }
+}
